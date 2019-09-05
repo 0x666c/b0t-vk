f3b13a3b34f18a5cd258d7e22c509119c7541064
0x666c <0x666c@mail.ru>
2019-09-05 21:05:11 +0300

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': 0})
+
+# API-ключ созданный ранее
+token = "646f8b5924863bbe7e28bc28ac3ad5f1de3c31b44b800f5b3fb504fcd961da09d62c9c9a0caef29b05330"
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+# Основной цикл
+for event in longpoll.listen():
+
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
+    
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+        
+            # Сообщение от пользователя
+            request = event.text
+            
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
